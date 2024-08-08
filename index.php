93cc23ef330f6ab51757194dbad8d790409ab9a3
GitteChristensen <119286746+GitteChristensen@users.noreply.github.com>
2024-08-08 08:32:17 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
+    <title>new cro users</title>
+</head>
+
+<body>
+    <h3>Signup Cro User</h3>
+
+    <form action="formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="firstname" placeholder="Firstname">
+        <input type="text" name="lastname" placeholder="Lastname">
+        <input type="password" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="Email">
+        <button type="submit">Confirm Signup</button>       
+    </form>
+</body>
+</html>
